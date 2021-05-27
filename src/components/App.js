78bdbe23064e718f4bdcbbb87fbb0ef369871254
Yuanming Chai <yaunmingchai@gmail.com>
2021-05-27 14:18:36 -0400

    convert to functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import SearchBar from './Searchbar';
 import youtube from '../apis/youtube';
 import unsplash from "../apis/unsplash";
@@ -6,16 +5,21 @@ import ImageList from "./ImageList";
 import VideoList from './VideoList';
 import Grid from "@material-ui/core/Grid";
 import 'semantic-ui-css/semantic.min.css';
+import React, { useState, useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
-class App extends React.Component {
-    state = {
-        videos: [],
-        images: [],
-        collection: [],
-        selectedItems: {},
-        term: null,
-    }
-    handleSubmit = async (termFromSearchBar) => {
+function App () {
+    const [videos, setvideos] = useState([])
+    const [images, setimages] = useState([])
+    const [selectedItems, setselectedItems] = useState({})
+    const [term, setTerm] = useState('')
+
+    const handleSubmit = async (termFromSearchBar) => {
+        setselectedItems({})
+        setTerm(termFromSearchBar)
+        console.log(typeof termFromSearchBar)
+        debugger
         const response = await youtube.get('/search', {
             params: {
                 q: termFromSearchBar
@@ -27,66 +31,161 @@ class App extends React.Component {
                per_page: 7
             },
          });
-        this.setState({
-            videos: response.data.items,
-            images: imageresponse.data.results,
-            term: termFromSearchBar,
-        })
+        setvideos(response.data.items);
+        setimages(imageresponse.data.results);
+        setTerm(termFromSearchBar);
     };
-    handleVideoSelect = (video) => {
-        var term = this.state.term;
-        if (!this.state.selectedItems[term]){
-            this.state.selectedItems[term] = [`https://www.youtube.com/embed/${video.id.videoId}`];
+    const handleVideoSelect = async (video) => {
+        if (!selectedItems[term]){
+            selectedItems[term] = [`https://www.youtube.com/embed/${video.id.videoId}`];
         }
         else{
-            this.state.selectedItems[term].push(`https://www.youtube.com/embed/${video.id.videoId}`);
+            selectedItems[term].push(`https://www.youtube.com/embed/${video.id.videoId}`);
+            setselectedItems(selectedItems)
         }
-        this.setState({selectedItems: this.state.selectedItems});
+        setselectedItems(selectedItems)
     }
 
-    handleImageSelect = (image) => {
-        var term = this.state.term;
-        if (!this.state.selectedItems[term]){
-            this.state.selectedItems[term] = [image.links.download];
+    const handleImageSelect = async (image) => {
+        if (!selectedItems[term]){
+            selectedItems[term] = [image.links.download];
         }
         else{
-            this.state.selectedItems[term].push(image.links.download);
+            selectedItems[term].push(image.links.download);
+            setselectedItems(selectedItems)
         }
-        this.setState({selectedItems: this.state.selectedItems});
+        setselectedItems(selectedItems)
+        console.log(selectedItems)
     }
 
-    handleClick () {
-        // console.log("hi");
-        // console.log(this.state);
+    const handleClick = () => {
+        setselectedItems({})
     }
 
-    render() {
         return (
-            <div className='ui container' style={{marginTop: '1em'}}>
-                <SearchBar handleFormSubmit={this.handleSubmit}/>
+            <div style={{marginTop: '1em'}}>
+                <Container>
+                    <SearchBar handleFormSubmit={handleSubmit}/>
                     <div>
                         <Grid container spacing={3}>
                             <Grid item xs={4}>
-                                <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(0,3)} />
+                                <ImageList handleVideoSelect={handleImageSelect} images={images.slice(0,3)} />
                             </Grid>
                             <Grid item xs={4}>
-                                <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(3,6)} />
+                                <ImageList handleVideoSelect={handleImageSelect} images={images.slice(3,6)} />
                             </Grid>
                             <Grid item xs={4}>
-                                <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/>
-                                <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(6,7)} />
+                                <VideoList handleVideoSelect={handleVideoSelect} videos={videos}/>
+                                <ImageList handleVideoSelect={handleImageSelect} images={images.slice(6,7)} />
                             </Grid>
                         </Grid>
-                        <button onClick={this.handleClick}>
+                        <Button style={{marginTop: '1em'}} variant="contained" color="primary" onClick={handleClick}>
                             Clear saved
-                        </button>
-                        <div>
-                              <p>Selected Items: {JSON.stringify(this.state.selectedItems)}</p>
+                        </Button>
+                        <div style={{marginTop: '1em'}}>
+                              <p>Selected Items: {JSON.stringify(selectedItems)}</p>
                         </div>
-                </div>
+                    </div>
+                </Container>
             </div>
         )
-    }
 }
 
 export default App;
+
+// class App extends React.Component {
+//     constructor () {
+//         super()
+//         this.state = {
+//             videos: [],
+//             images: [],
+//             collection: [],
+//             selectedItems: {},
+//             term: null,
+//         }
+//     }
+//     // state = {
+//     //     videos: [],
+//     //     images: [],
+//     //     collection: [],
+//     //     selectedItems: {},
+//     //     term: null,
+//     // }
+//     handleSubmit = async (termFromSearchBar) => {
+//         const response = await youtube.get('/search', {
+//             params: {
+//                 q: termFromSearchBar
+//             }
+//         })
+//         const imageresponse = await unsplash.get("/search/photos", {
+//             params: {
+//                query: termFromSearchBar,
+//                per_page: 7
+//             },
+//          });
+//         console.log(termFromSearchBar)
+//         this.setState({
+//             videos: response.data.items,
+//             images: imageresponse.data.results,
+//             term: termFromSearchBar,
+//         })
+//     };
+//     handleVideoSelect = (video) => {
+//         var term = this.state.term;
+//         if (!this.state.selectedItems[term]){
+//             this.state.selectedItems[term] = [`https://www.youtube.com/embed/${video.id.videoId}`];
+//         }
+//         else{
+//             this.state.selectedItems[term].push(`https://www.youtube.com/embed/${video.id.videoId}`);
+//         }
+//         this.setState({selectedItems: this.state.selectedItems});
+//     }
+
+//     handleImageSelect = (image) => {
+//         var term = this.state.term;
+//         if (!this.state.selectedItems[term]){
+//             this.state.selectedItems[term] = [image.links.download];
+//         }
+//         else{
+//             this.state.selectedItems[term].push(image.links.download);
+//         }
+//         this.setState({selectedItems: this.state.selectedItems});
+//         console.log(this.state.selectedItem)
+//     }
+
+//     handleClick () {
+//         this.setState({
+//             selectedItems: {}
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className='ui container' style={{marginTop: '1em'}}>
+//                 <SearchBar handleFormSubmit={this.handleSubmit}/>
+//                     <div>
+//                         <Grid container spacing={3}>
+//                             <Grid item xs={4}>
+//                                 <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(0,3)} />
+//                             </Grid>
+//                             <Grid item xs={4}>
+//                                 <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(3,6)} />
+//                             </Grid>
+//                             <Grid item xs={4}>
+//                                 <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/>
+//                                 <ImageList handleVideoSelect={this.handleImageSelect} images={this.state.images.slice(6,7)} />
+//                             </Grid>
+//                         </Grid>
+//                         <button onClick={this.handleClick}>
+//                             Clear saved
+//                         </button>
+//                         <div>
+//                               <p>Selected Items: {JSON.stringify(this.state.selectedItems)}</p>
+//                         </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
-class Searchbar extends React.Component {
-    state = {
-        term: 'Default text'
+function Searchbar(props) {
+    const [input, setInput] = useState('Default Text')
+
+    const handleChange = event => {
+        // console.log(event.target.value);
+        setInput(event.target.value);
     };
-    handleChange = (event) => {
-        this.setState({
-            term: event.target.value
-        });
-    };
-    handleSubmit = event => {
+
+    const handleSubmit = event => {
+        setInput(input);
         event.preventDefault();
-        this.props.handleFormSubmit(this.state.term);
+        props.handleFormSubmit(input);
     }
 
-    render() {
         return (
             <div className='search-bar ui segment'>
-                <form onSubmit={this.handleSubmit} className='ui form'>
+                <form onSubmit={handleSubmit} className='ui form'>
                     <div className='field'>
-                        <label htmlFor="video-search">Image&Video Search</label>
-                        <input onChange={this.handleChange} name='video-search' type="text" value={this.state.term}/>
+                        <label htmlFor="video-search">Image & Video Search</label>
+                        <input onChange={handleChange} value={input} name='video-search' type="text" input={input}/>
                     </div>
                 </form>
             </div>
         )
-    }
 }
 export default Searchbar;
+
+// class Searchbar extends React.Component {
+//     state = {
+//         term: 'Default text'
+//     };
+//     handleChange = (event) => {
+//         this.setState({
+//             term: event.target.value
+//         });
+//     };
+//     handleSubmit = event => {
+//         event.preventDefault();
+//         this.props.handleFormSubmit(this.state.term);
+//     }
+
+//     render() {
+//         return (
+//             <div className='search-bar ui segment'>
+//                 <form onSubmit={this.handleSubmit} className='ui form'>
+//                     <div className='field'>
+//                         <label htmlFor="video-search">Image&Video Search</label>
+//                         <input onChange={this.handleChange} name='video-search' type="text" value={this.state.term}/>
+//                     </div>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+// export default Searchbar;
